fdab209126d96bc9a9b9dd45d704253e7c263732
nihalkumar2906 <74289117+nihalkumar2906@users.noreply.github.com>
2020-12-22 13:00:35 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+var tom1 = new Audio("sounds/tom-1.mp3");
+var tom2 = new Audio("sounds/tom-2.mp3");
+var tom3 = new Audio('sounds/tom-3.mp3');
+var tom4 = new Audio('sounds/tom-4.mp3');
+var snare = new Audio('sounds/snare.mp3');
+var crash = new Audio('sounds/crash.mp3');
+var kick = new Audio('sounds/kick-bass.mp3');
+
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
@@ -10,7 +18,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 document.addEventListener("keydown", function(event) {
   var pressedKey = (event.key).toLowerCase();
-  if(pressedKey === "w" || pressedKey === "a" || pressedKey === "s" || pressedKey === "d" || pressedKey === "j" || pressedKey === "k" || pressedKey === "l") {
+  if (pressedKey === "w" || pressedKey === "a" || pressedKey === "s" || pressedKey === "d" || pressedKey === "j" || pressedKey === "k" || pressedKey === "l") {
     makeSound(pressedKey);
     buttonAnimation(pressedKey);
   }
@@ -20,41 +28,35 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
     case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
     case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
 
     case "d":
-      var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
 
     case "j":
-      var snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
     case "k":
-      var crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
 
     case "l":
-      var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
 
-    default: console.log(key);
+    default:
+      console.log(key);
 
   }
 }
